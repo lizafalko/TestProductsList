e3c13622d806f94154a4355b72f091680780d338
elizaveta.falko <lizafloyd2@gmail.com>
2022-09-15 14:48:33 +0700

    added new files

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>This is ToDo Application</h1>
+    <hr />
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Купить хлеб", completed: false },
+        { id: 2, title: "Купить кокосовое масло", completed: false },
+        { id: 3, title: "Купить оливки", completed: false },
+      ],
+    };
+  },
+  components: { TodoList },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((item) => item.id !== id);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <li>
+    <span v-bind:class="{ done: todo.completed }">
+      <input
+        type="checkbox"
+        v-on:change="markComplete(todo.completed)"
+        v-bind:checked="todo.completed"
+      />
+      <strong>{{ todo.id }}</strong>
+      {{ todo.title }}
+    </span>
+    <button class="button" v-on:click="$emit('remove-todo', todo.id)">
+      &times;
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    markComplete(isComplete) {
+      this.todos.completed = !isComplete;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  border: 1px solid pink;
+  justify-content: space-between;
+  padding: 0.5rem 2rem;
+  margin-bottom: 1rem;
+}
+
+.button {
+  border: none;
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 600;
+  background: pink;
+  padding: 10px 20px;
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+.input {
+  margin-right: 1rem;
+}
+</style>
